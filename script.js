390344ea611f26f66d4804fe8ab8087144c1e339
Firdavs <usmonovfirdavs100@gmail.com>
2024-01-28 18:14:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var yourName = prompt('веедите своё имя')
+// alert('Привет  ' + yourName)
+var yourAge = prompt('Введите свой возраст')
+// alert('вам ' + yourAge + ' лет/года')
+var plus = prompt('Решите пример 6 + 3 = ?')
+var minus = prompt('Решите пример 20 - 5 = ?')
+var multiplication = prompt('Решите пример 15 * 2 = ?')
+var division = prompt('Решите пример 8 / 4 = ?')
+var percent = prompt('Решите пример 10 % 3 = ?')
+alert('Откройте консоль')
+console.log('Меня зовут ' + yourName);
+console.log('Мне ' + yourAge);
+console.log('Пример 1:  6 + 3 = 9 Ваш ответ ' + plus);
+console.log('Пример 2:  20 - 5 = 15 Ваш ответ ' + minus);
+console.log('Пример 3:  15 * 2 = 30 Ваш ответ ' + multiplication);
+console.log('Пример 4:  8 / 4 = 2 Ваш ответ ' + division);
+console.log('Пример 5:  10 % 3 = 1 Ваш ответ ' + percent);
